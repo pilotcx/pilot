82c342fff1ca1958b04474c44a415769a58c8f8a
Edward Nguyen <nhanna@northstudio.vn>
2025-04-13 06:43:10 +0700

    :art: team general navigations

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -35,7 +35,7 @@ export function NavMain({
 }) {
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>Platform</SidebarGroupLabel>
+      <SidebarGroupLabel>Workflow</SidebarGroupLabel>
       <SidebarMenu>
         {items.map((item) => (
           <Collapsible
