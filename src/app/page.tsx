5e2e1e4eeab060e67e96cc5725f59049c0411bc0
Thắng Nguyễn <thangdevalone@gmail.com>
2025-04-13 22:02:23 +0700

    🚀 Resolve build fail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,8 +43,10 @@ export default async function HomePage() {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.displayName} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  <AvatarImage src={user.avatar} alt={user.fullName} />
+                  <AvatarFallback className="rounded-lg">
+                    {user?.fullName.slice(0, 2)}
+                  </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">{user.fullName}</span>
