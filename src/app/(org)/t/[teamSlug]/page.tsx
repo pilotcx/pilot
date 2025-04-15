c758ddaa44aaf300abf4285d0f2a5420e77cdf65
Edward Nguyen <nhanna@northstudio.vn>
2025-04-16 04:53:53 +0700

    :bug: fix team not found on team provider

--- src/app/(org)/t/[teamSlug]/page.tsx
+++ src/app/(org)/t/[teamSlug]/page.tsx
@@ -1,9 +1,6 @@
-import withTeam from "@/lib/utils/withTeam";
 import {TeamNewsfeed} from "@/app/(org)/t/[teamSlug]/components/newsfeed";
 
 export default async function TeamPage({params}: { params: any }) {
-  const {team} = await withTeam((await params).teamSlug);
-
   return (
     <div className="container max-w-3xl mx-auto">
       <TeamNewsfeed/>
