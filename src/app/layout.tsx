1a3aa928caf680532a78c1d60bd59f7706fc0f58
Edward Nguyen <nhanna@northstudio.vn>
2025-04-16 14:52:16 +0700

    :construction_worker: global layout with user configured metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,43 @@
-import {Inter} from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import {ReactNode} from "react";
-import {Toaster} from "sonner";
+import { ReactNode } from "react";
+import { systemConfigService } from "@/lib/services/system-config";
+import { SystemConfigKey } from "@/lib/types/models/system-config";
+import { dbService } from "@/lib/db/service";
+import { Toaster } from "sonner";
 
 const interFont = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
 
-export default async function RootLayout({children}: { children: ReactNode }) {
+export async function generateMetadata(): Promise<Metadata> {
+  await dbService.connect();
+  const title = await systemConfigService.get<string>(SystemConfigKey.OrgName, "Tower");
+  const description = await systemConfigService.get<string>(
+    SystemConfigKey.OrgDesc
+  );
+
+  return {
+    title,
+    description,
+  };
+}
+
+export default async function RootLayout({ children }: { children: ReactNode }) {
+  await dbService.connect();
+  const avatar = await systemConfigService.get<string>(SystemConfigKey.OrgAvatar);
+  const title = await systemConfigService.get<string>(SystemConfigKey.OrgName, "Tower");
   return (
     <html lang="en">
+    <head>
+      <title>{title}</title>
+      <link rel="icon" href={avatar ?? "/favicon.ico"} sizes="any" />
+    </head>
     <body className={`${interFont.variable} antialiased`}>
     {children}
-    <Toaster position="top-right"/>
+    <Toaster position="top-right" />
     </body>
     </html>
   );
