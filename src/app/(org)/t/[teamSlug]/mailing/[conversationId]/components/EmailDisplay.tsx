f1c4b6af3918127658c9afa8bb7a109906e650bb
Edward Nguyen <nhan@northstudio.vn>
2025-04-28 20:14:25 +0700

    :sparkles: view email metadata #7

--- src/app/(org)/t/[teamSlug]/mailing/[conversationId]/components/EmailDisplay.tsx
+++ src/app/(org)/t/[teamSlug]/mailing/[conversationId]/components/EmailDisplay.tsx
@@ -3,11 +3,13 @@ import EmailIframe from "@/app/(org)/t/[teamSlug]/mailing/[conversationId]/compo
 import gravatar from "gravatar";
 import dayjs from "dayjs";
 import {parseEmailFrom} from "@/lib/utils/parseEmailFrom";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import {ChevronDownIcon} from "lucide-react";
 
 export default function EmailDisplay({email}: { email: Email }) {
   const {name: senderName, email: senderEmail} = parseEmailFrom(email.from);
-  let recipient = email.recipient?.split('@')[0];
-  if (email.direction === EmailType.Outgoing) recipient = email.to.map(x => x.split('@')[0]).join(', ');
+  let recipient = [email.recipient];
+  if (email.direction === EmailType.Outgoing) recipient = email.to;
 
   return <div>
     <div className={'flex flex-row gap-4 px-4 mb-2'}>
@@ -32,8 +34,51 @@ export default function EmailDisplay({email}: { email: Email }) {
             {dayjs(email.createdAt).format('MMM D, YYYY h:mm A')}
           </div>
         </div>
-        <div className={'text-muted-foreground text-xs'}>
-          to {recipient}
+        <div className={'flex flex-row items-center'}>
+          <div className={'text-muted-foreground text-xs'}>
+            to {recipient.map(r => r.split('@')[0]).join(', ')}
+          </div>
+          <Popover>
+            <PopoverTrigger asChild>
+              <div className={'p-0.5 cursor-pointer'}>
+                <ChevronDownIcon className={'size-3'}/>
+              </div>
+            </PopoverTrigger>
+            <PopoverContent className={'text-xs w-full'}>
+              <div className={'flex flex-row gap-2'}>
+                <div className={'w-16 text-right text-muted-foreground'}>
+                  From:
+                </div>
+                <div className={'flex-1 font-medium line-clamp-1'}>
+                  {email.from}
+                </div>
+              </div>
+              <div className={'flex flex-row gap-2'}>
+                <div className={'w-16 text-right text-muted-foreground'}>
+                  To:
+                </div>
+                <div className={'font-medium'}>
+                  {recipient.join(', ')}
+                </div>
+              </div>
+              <div className={'flex flex-row gap-2'}>
+                <div className={'w-16 text-right text-muted-foreground'}>
+                  Date:
+                </div>
+                <div className={'font-medium whitespace-nowrap'}>
+                  {dayjs(email.createdAt).format('MMM D, YYYY h:mm A')}
+                </div>
+              </div>
+              <div className={'flex flex-row gap-2'}>
+                <div className={'w-16 text-right text-muted-foreground'}>
+                  Subject:
+                </div>
+                <div className={'font-medium whitespace-nowrap'}>
+                  {email.subject}
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
         </div>
       </div>
     </div>
